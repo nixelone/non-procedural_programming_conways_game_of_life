bd09c3adde5f14c9a61741430586286147b8704d
Nikolas Hamarik <nikolas.hamarik@rossum.ai@Nikolas--MacBook-Pro.local>
2025-08-31 11:43:42 +0200

    added first iteration - basic grid drawn on the screen

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,43 @@
-module Main (main) where
+import Graphics.Gloss
 
+-- Represent the world
+data World = World
+  { grid     :: [[Bool]]
+  , cellSize :: Float
+  }
+
+-- Example grid: a vertical blinker
+initialWorld :: World
+initialWorld = World
+  { grid = [ [False, True,  False]
+           , [False, True,  True]
+           , [False, True,  False]
+           ]
+  , cellSize = 50
+  }
+
+-- Convert World -> Picture
+render :: World -> Picture
+render (World g size) = Pictures $
+  [ translate x y (color (cellColor alive) (rectangleSolid size size))
+  | (row, j) <- zip g [0..]
+  , (alive, i) <- zip row [0..]
+  , let x = fromIntegral i * size - halfWidth
+  , let y = fromIntegral (negate j) * size + halfHeight
+  ]
+  where
+    rows = length g
+    cols = length (head g)
+    halfWidth  = fromIntegral (cols - 1) * size / 2
+    halfHeight = fromIntegral (rows -1) * size / 2
+
+cellColor :: Bool -> Color
+cellColor True  = white
+cellColor False = black
+
+-- Main just displays the initial world
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = display
+         (InWindow "Game of Life" (400, 200) (300, 300))
+         (greyN 0.8)
+         (render initialWorld)
